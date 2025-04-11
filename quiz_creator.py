8fdf097cebd4ab6d7571d28ab13de7756ee115c9
Rafael-Ian <cabertesilorio@gmai.com>
2025-04-11 19:06:34 +0800

    more definitions and description changes

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,6 +1,9 @@
 #Assignment 9: Quiz Creator
 
-# The program will ask the user to input questions that he needs for the quiz.
-# Next, the program will ask the user to input the choices that he wants for the quiz.
+# The program will ask the user to input questions that the user needs for the quiz.
+# Next, the program will ask the user to input the choices (a,b,c,d) that the user wants for the quiz.
 # Then, the program will ask the user to input the correct answer for that specific question.
+# If the input is valid, then it will break out of the loop.
+# Invalid inputs will keep the loop.
 # Next, the program will ask the user if he wants to input another question by yes or no.
+# The program will exit if the user doesn't want to continue or doesn't need to add any more questions.
