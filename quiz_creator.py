b640e73d0045332ec68d596aaee8b3940244a6ff
Rafael-Ian <cabertesilorio@gmai.com>
2025-04-11 19:00:24 +0800

    program definitions

---
+++ quiz_creator.py
@@ -0,0 +1,6 @@
+#Assignment 9: Quiz Creator
+
+# The program will ask the user to input questions that he needs for the quiz.
+# Next, the program will ask the user to input the choices that he wants for the quiz.
+# Then, the program will ask the user to input the correct answer for that specific question.
+# Next, the program will ask the user if he wants to input another question by yes or no.
