42b6043326e1dd47d9f7f0e640745e1807af0b37
Rafael-Ian <cabertesilorio@gmai.com>
2025-04-11 19:13:15 +0800

    another description addition and code introo

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,5 +1,10 @@
 #Assignment 9: Quiz Creator
 
+def main():
+    # open the file called "qz_qstns.txt" in append mode which is "a", meaning new questions will be added without removing those existing ones
+    with open("qz_qstns.txt", "a")as file:
+        while True:
+            print("\n ___ Quiz Questions Creator ___")
 # The program will ask the user to input questions that the user needs for the quiz.
 # Next, the program will ask the user to input the choices (a,b,c,d) that the user wants for the quiz.
 # Then, the program will ask the user to input the correct answer for that specific question.
